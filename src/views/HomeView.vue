d04cd38c3883e4fe0c5939edcd0c17c0d87c1c4b
Lazeon29 <sophiacalimbo@gmail.com>
2025-06-10 11:58:45 +0800

    feat: added HomeView

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <h1>Trash Detector</h1>
+    <CameraCapture />
+  </div>
+</template>
+
+<script setup>
+import CameraCapture from '../components/CameraCapture.vue'
+</script>
